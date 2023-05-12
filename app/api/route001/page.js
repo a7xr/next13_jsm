80858a378307e20928a5ff20a66a85dcbb17ae72
Mamitiana <mamitiana@fulldigits.com>
2023-05-12 22:36:04 +0300

    46:34 Deleted the app/, going to rebuild it

--- app/api/route001/page.js
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Route001",
-};
-
-export default function Page() {
-  return <h1>This is Route001</h1>;
-}
--- app/posts/new/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Posts/new</div>;
-};
-
-export default page;
