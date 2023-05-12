c316e30ad2a4a66d0289ad1146b2b958b08a7dfe
Mamitiana <mamitiana@fulldigits.com>
2023-05-13 01:05:12 +0300

    56:20 Starting on Front, better that you know TailwindCSS

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center"> 
+            Discover and Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+        </h1>
+        <p className="desc text-center">
+            Long text here
+        </p>
+    </section>
   )
 }
 
