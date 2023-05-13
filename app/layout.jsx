1a2dda77e7f8fc6c60aeed190f403897ddaaafa8
Mamitiana <mamitiana@fulldigits.com>
2023-05-13 21:11:37 +0300

    1:17:22 Include compProvider into app/layout.js; You have to login, and when you are loggedIn, there is session... This chg is going to mng that Session

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,14 +13,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-  
-        <main className="app">
-          <Nav></Nav>
-          {children} {/* */}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+    
+          <main className="app">
+            <Nav></Nav>
+            {children} {/* */}
+          </main>
+        </Provider>
       </body>
     </html>
   );
