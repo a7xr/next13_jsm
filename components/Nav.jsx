841cce1ee89e2796d14166c09c760af00457b2e9
Mamitiana <mamitiana@fulldigits.com>
2023-05-13 06:47:39 +0300

    1:2:37 Note the way width=30px is being set, <Image /> Must have propWidth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,12 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
+        <Image 
+          src="/assets/images/logo.svg"
+          width={30}
+          height={30}
+          className="object-contain"
+        ></Image>
       </Link>
     </nav>
   )
