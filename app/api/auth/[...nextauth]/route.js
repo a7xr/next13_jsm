092543d239d3ed61656310c91573b9648d0984fb
Mamitiana <mamitiana@fulldigits.com>
2023-05-13 21:52:17 +0300

    1:24:24 importing the google_credentials into Back_Auth_API + Formatting + Printing that googleCred, MoreInfo dr_500

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId: '',
-            clientSecret: ''
-        })
-    ],
-    async session ({session}){
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 
-    },
-    async signIn ({profile}){
-        
-    },
-})
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
