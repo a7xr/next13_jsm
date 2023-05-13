dec50a5ae93950d5212b97bc9631145f4a09e0f2
Mamitiana <mamitiana@fulldigits.com>
2023-05-13 06:45:09 +0300

    1:2:37 compNav.jsx import Link+Image, that is going to be our site_logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,15 @@
+
+import Link from "next/link";
+import Image from "next/image";
+
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+      </Link>
+    </nav>
   )
 }
 
