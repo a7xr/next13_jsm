c611ebed57365caa41fbdda192268a7d4fdc4e11
Mamitiana <mamitiana@fulldigits.com>
2023-05-21 14:06:14 +0300

    1:52:45 Done with front of /create-prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -8,9 +8,22 @@ import Form from '@components/Form';
 
 const CreatePrompt = () => {
   const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  })
+
+  const createPrompt = async(e) => {
+
+  }
+
   return (
     <Form
-
+      type= "Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     >
 
     </Form>
