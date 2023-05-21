3dc19b5dcc819e407e88ae043e808d9e9116b1b1
Mamitiana <mamitiana@fulldigits.com>
2023-05-21 16:52:13 +0300

    2:17:0 there is an <input /> in the /

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,30 @@
+"use client";
+
+import {useState, useEffect} from 'react';
+
+import PromptCard from './PromptCard';
+
 import React from "react";
 
 const Feed = () => {
-  return <div>Feed</div>;
+    const [searchText, setSearchText] = useState('');
+    const handleSearchChange = (e) => {
+        
+    }
+    return (
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input 
+                    placeholder='Search for a tag or a username'
+                    type="text" 
+                    className="search_input peer" 
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                />
+            </form>
+        </section>
+    )
 };
 
 export default Feed;
