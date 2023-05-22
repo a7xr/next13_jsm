6a7649214ffab87cd6883af6ba1cb702cad9719d
Mamitiana <mamitiana@fulldigits.com>
2023-05-22 04:36:49 +0300

    2:35:30 /profile is basically working

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { useState, useEffect} from 'react';
+import {useSession} from 'next-auth/react';
+import {useRouter} from 'next/navigation';
+
+import Profile from '@components/Profile';
+
 import React from 'react'
 
-const Profile = () => {
+const MyProfile = () => {
+  const handleEdit = () => {}
+  const handleDelete = () => {}
+
   return (
-    <div>Profile</div>
+    <Profile 
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete} 
+    />
   )
 }
 
-export default Profile
+export default MyProfile
