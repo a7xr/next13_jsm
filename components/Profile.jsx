35b5b1992dbcb39e550160df02eb6fd2fd531ac5
Mamitiana <mamitiana@fulldigits.com>
2023-05-22 05:04:47 +0300

    2:43:21 /profile is displaying more info

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,8 +3,13 @@ import PromptCard from "./PromptCard";
 const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
   return (
     <section className="w-full">
-        <h1 className="x">
-            {name} Profile
+        <h1 className="head_text text-left">
+            <span className="blue_gradient">
+                {name} Profile
+            </span>
+            <p className="desc text-left">
+                {desc }
+            </p>
         </h1>
     </section>
   );
