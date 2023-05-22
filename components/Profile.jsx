5dfc0f4f710073ee385a16e88b21a7e500c3db36
Mamitiana <mamitiana@fulldigits.com>
2023-05-22 04:59:08 +0300

    2:41:04 /profile is showing "My Profile", it is grabbing data that is passed from api/[id]/posts/route.js

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import PromptCard from "./PromptCard";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
+  return (
+    <section className="w-full">
+        <h1 className="x">
+            {name} Profile
+        </h1>
+    </section>
+  );
 };
 
 export default Profile;
